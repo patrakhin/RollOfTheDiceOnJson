ed00fe5fb93a95d8a403690a3da397c6890d9950
patrakhin <patrakhin@mail.ru>
2023-06-25 15:51:39 +0300

    Initial commit

---
+++ src/main/java/ru/patrakhin/RollOfTheDiceOnJson/RollOfTheDiceOnJsonApplication.java
@@ -0,0 +1,13 @@
+package ru.patrakhin.RollOfTheDiceOnJson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollOfTheDiceOnJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollOfTheDiceOnJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/patrakhin/RollOfTheDiceOnJson/RollOfTheDiceOnJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patrakhin.RollOfTheDiceOnJson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollOfTheDiceOnJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
